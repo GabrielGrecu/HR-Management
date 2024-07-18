c798cd479be52ae659dfda020227283c3b6c3f91
Gabriel Grecu <grecualexandrugabriel@yahoo.com>
2024-07-18 18:49:22 +0300

    update CandidateValidator with Object methods

--- HR-Management/candidate-management/src/main/java/com/nagarro/si/cm/util/CandidateValidator.java
+++ HR-Management/candidate-management/src/main/java/com/nagarro/si/cm/util/CandidateValidator.java
@@ -18,15 +18,12 @@ public class CandidateValidator {
         this.candidateRepository = candidateRepository;
     }
 
-    public boolean updateCandidateFields(Candidate candidate, CandidateDto candidateDto) throws ParseException {
-        boolean changes = false;
-
+    public void updateCandidateFields(Candidate candidate, CandidateDto candidateDto) throws ParseException {
         if (candidateDto.getUsername() != null && !candidateDto.getUsername().equals(candidate.getUsername())) {
             if (candidateRepository.existsCandidateByUsername(candidateDto.getUsername())) {
                 throw new DuplicateResourceException("Username already taken");
             }
             candidate.setUsername(candidateDto.getUsername());
-            changes = true;
         }
 
         if (candidateDto.getBirthday() != null && !candidateDto.getBirthday().equals(candidate.getBirthday())) {
@@ -35,7 +32,6 @@ public class CandidateValidator {
             java.sql.Date sqlDate = new java.sql.Date(parsedDate.getTime());
 
             candidate.setBirthday(sqlDate);
-            changes = true;
         }
 
         if (candidateDto.getEmail() != null && !candidateDto.getEmail().equals(candidate.getEmail())) {
@@ -43,17 +39,14 @@ public class CandidateValidator {
                 throw new DuplicateResourceException("Email already taken");
             }
             candidate.setEmail(candidateDto.getEmail());
-            changes = true;
         }
 
         if (candidateDto.getCity() != null && !candidateDto.getCity().equals(candidate.getCity())) {
             candidate.setCity(candidateDto.getCity());
-            changes = true;
         }
 
         if (candidateDto.getFaculty() != null && !candidateDto.getFaculty().equals(candidate.getFaculty())) {
             candidate.setFaculty(candidateDto.getFaculty());
-            changes = true;
         }
 
         if (candidateDto.getPhoneNumber() != null && !candidateDto.getPhoneNumber().equals(candidate.getPhoneNumber())) {
@@ -61,19 +54,14 @@ public class CandidateValidator {
                 throw new DuplicateResourceException("Phone number already taken");
             }
             candidate.setPhoneNumber(candidateDto.getPhoneNumber());
-            changes = true;
         }
 
         if (candidateDto.getYearsOfExperience() != 0 && candidateDto.getYearsOfExperience() != candidate.getYearsOfExperience()) {
             candidate.setYearsOfExperience(candidateDto.getYearsOfExperience());
-            changes = true;
         }
 
         if (candidateDto.getRecruitmentChannel() != null && !candidateDto.getRecruitmentChannel().equals(candidate.getRecruitmentChannel())) {
             candidate.setRecruitmentChannel(candidateDto.getRecruitmentChannel());
-            changes = true;
         }
-
-        return changes;
     }
 }
