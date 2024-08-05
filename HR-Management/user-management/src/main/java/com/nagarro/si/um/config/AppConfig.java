389d2a950d5231a5b09106cbce701d8bb62ecefa
DianaHategan11 <diana.hategan1107@gmail.com>
2024-08-05 16:25:36 +0300

    Implemented AuthenticationConfig class

--- HR-Management/user-management/src/main/java/com/nagarro/si/um/config/AppConfig.java
+++ HR-Management/user-management/src/main/java/com/nagarro/si/um/config/AppConfig.java
@@ -2,26 +2,13 @@ package com.nagarro.si.um.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.nagarro.si.um.exception.EntityNotFoundException;
-import com.nagarro.si.um.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.net.http.HttpClient;
 
 @Configuration
-@RequiredArgsConstructor
 public class AppConfig {
-
-    private final UserRepository userRepository;
 
     @Bean
     public HttpClient httpClient() {
@@ -33,29 +20,5 @@ public class AppConfig {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
         return objectMapper;
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return email -> userRepository.findByEmail(email)
-                .orElseThrow(() -> new EntityNotFoundException(String.format("User with email %s not found", email)));
-    }
-
-    @Bean
-    public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(userDetailsService());
-        authenticationProvider.setPasswordEncoder(passwordEncoder());
-        return authenticationProvider;
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-        return config.getAuthenticationManager();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
